ac8fda6cde94daace7fc8e4025ba7547b3fd7f33
TommyLei07 <827171530@qq.com>
2020-04-13 22:46:06 +0800

    test_2

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,6 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hello world--This is for testing git from local idea");
+        System.out.println("This is the second C&P test");
     }
 }
